3bccaea161a986f571ba6db497816a2292275fc6
Nataliia Steblyna <67156001+steblinka@users.noreply.github.com>
2022-02-23 20:03:22 +0300

    Add files via upload

---
+++ mentions_rating.py
@@ -0,0 +1,145 @@
+import re
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_february.txt', encoding='utf-8') #txt file 
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------FEB------------------')
+print('Political Actor', pro)
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_march.txt', encoding='utf-8') #txt file 
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------MAR------------------')
+print('Political Actor', pro)
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_april.txt', encoding='utf-8') #txt file
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------APR------------------')
+print('Political Actor', pro)
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_may.txt', encoding='utf-8') #txt file 
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------MAY------------------')
+print('Political Actor', pro)
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_june.txt', encoding='utf-8') #txt file 
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------JUN------------------')
+print('Political Actor', pro)
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_july.txt', encoding='utf-8') #txt file 
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------JUL------------------')
+print('Political Actor', pro)
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_august.txt', encoding='utf-8') #txt file 
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------AUG------------------')
+print('Political Actor', pro)
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_september.txt', encoding='utf-8') #txt file 
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------SEP------------------')
+print('Political Actor', pro)
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_october.txt', encoding='utf-8') #txt file 
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------OCT------------------')
+print('Political Actor', pro)
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_november.txt', encoding='utf-8') #txt file 
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------NOV------------------')
+print('Political Actor', pro)
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_december.txt', encoding='utf-8') #txt file 
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------DEC------------------')
+print('Political Actor', pro)
+
+exampleFile = open(r'E:\html_pravda\texts\2021\dt\dt_headlines\dt_january.txt', encoding='utf-8') #txt file 
+exampleFile1 = exampleFile.read()
+
+name_1 = re.compile(r'Зеленськ') #name
+head = name_1.findall(exampleFile1)
+headli = ('\n'.join(head))
+
+
+pro = headli.count('Зеленськ')#counts the number of mentions per day
+print('------------------JAN------------------')
+print('Political Actor', pro)
